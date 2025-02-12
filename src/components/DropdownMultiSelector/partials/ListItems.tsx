e5728528574682d3fb046d621ed4db4bf37cad27
Moh Sen <19666430+NaderiMo@users.noreply.github.com>
2025-02-12 23:01:19 +0330

    feat: animation + scroll style + max height of list item

--- src/components/DropdownMultiSelector/partials/ListItems.tsx
+++ src/components/DropdownMultiSelector/partials/ListItems.tsx
@@ -1,11 +1,25 @@
 import { FC, ReactNode } from "react";
+import { motion } from "motion/react";
 
 export interface Props {
   children: ReactNode;
 }
 
 const ListItem: FC<Props> = ({ children }) => {
-  return <div className="listItem">{children}</div>;
+  const transition = {
+    duration: 0.8,
+    ease: [0, 0.71, 0.2, 1.01],
+  };
+
+  return (
+    <motion.div
+      animate={{ y: 16 }}
+      transition={transition}
+      className="listItem"
+    >
+      <div className="container">{children}</div>
+    </motion.div>
+  );
 };
 
 export default ListItem;
