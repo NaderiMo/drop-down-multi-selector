3a632c6cd070576a95c16241943c92f5e226b81b
Moh Sen <19666430+NaderiMo@users.noreply.github.com>
2025-02-12 17:48:11 +0330

    feat: input base

--- src/components/DropdownMultiSelector/index.tsx
+++ src/components/DropdownMultiSelector/index.tsx
@@ -1,11 +1,28 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import "./styles.scss";
 import { Props } from "./types";
 
 const DropdownMultiSelector: FC<Props> = ({ placeholder, width }) => {
+  const [value, setValue] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+  const handleInput = (e: React.FormEvent<HTMLDivElement>) => {
+    const newMessage = (e.target as HTMLDivElement).innerText;
+    const trimmedMessage = newMessage?.trim() || "";
+    setValue(trimmedMessage);
+  };
   return (
     <div className="dropdown-multi-selector" style={{ width }}>
-      <div className="input">{placeholder}</div>
+      <div
+        className="input"
+        onInput={handleInput}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        contentEditable
+      >
+        {/* {placeholder} */}
+      </div>
+      {isFocused && <div>List Items</div>}
+      {/* {value} */}
     </div>
   );
 };
