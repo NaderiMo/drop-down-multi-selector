3157f55407ad17d4c9720c027b613ff2169ca0d9
Moh Sen <19666430+NaderiMo@users.noreply.github.com>
2025-02-12 17:32:01 +0330

    feat: theme structure and component sample props

--- src/components/DropdownMultiSelector/index.tsx
+++ src/components/DropdownMultiSelector/index.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import "./styles.scss";
+import { Props } from "./types";
 
-const DropdownMultiSelector: FC = () => {
+const DropdownMultiSelector: FC<Props> = ({ placeholder, width }) => {
   return (
-    <>
-      <div className="sample-text">drop down component</div>
-    </>
+    <div className="dropdown-multi-selector" style={{ width }}>
+      <div className="input">{placeholder}</div>
+    </div>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import DropdownMultiSelector from "./components/DropdownMultiSelector";
+import "./themes/default.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <DropdownMultiSelector />
+    <DropdownMultiSelector placeholder={"enter"} width={300} />
   </StrictMode>
 );
